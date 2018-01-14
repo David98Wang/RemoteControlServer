f45dfa3591f1db438a0ae79a3438baa33b417011
David Wang <whcdavidca@gmail.com>
2018-01-14 01:23:43 -0500

    Initial Commit

---
+++ RemoteControlServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+namespace RemoteControlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
