a8c9d87787bdea71697425c4b0bbf8029d960956
David Wang <whcdavidca@gmail.com>
2018-01-14 01:31:17 -0500

    Server up and running

--- RemoteControlServer/Program.cs
+++ RemoteControlServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebSocketSharp;
 using WebSocketSharp.Server;
 namespace RemoteControlServer
 {
@@ -10,7 +11,20 @@ namespace RemoteControlServer
     {
         static void Main(string[] args)
         {
-
+            var wssv = new WebSocketServer("ws://localhost:4848");
+            wssv.AddWebSocketService<Controller>("/Controller");
+            wssv.Start();
+            Console.WriteLine("Websocket server running on port " + wssv.Port.ToString());
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+    public class Controller : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Send("Message Received");
+            base.OnMessage(e);
         }
     }
 }
